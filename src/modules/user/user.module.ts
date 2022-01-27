9ff9d05a4e70a78f0f7be19f162265f2d40689d5
OPOII <41926039+OPOII@users.noreply.github.com>
2022-01-26 23:26:06 -0500

    creating migrations

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,4 +1,8 @@
+import { UserRepository } from './user.repository';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([UserRepository])]
+})
 export class UserModule {}
